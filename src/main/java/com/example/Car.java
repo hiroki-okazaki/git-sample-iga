d4649baee7df949f046e410364218dadedac047a
hiroki-okazaki <1031okazaki@gmail.com>
2019-08-01 15:14:28 +0900

    faaa

--- src/main/java/com/example/Car.java
+++ src/main/java/com/example/Car.java
@@ -4,5 +4,12 @@ public class Car {
 
      public static void main(String[] args) {
 		System.out.println("gitテスト");
+		System.out.println("gitテスト");
+		System.out.println("gitテスト");
+		System.out.println("gitテスト");
+		System.out.println("gitテスト");
+		System.out.println("gitテスト");
+		System.out.println("gitテスト");
+		System.out.println("gitテスト");
 	}
 }
